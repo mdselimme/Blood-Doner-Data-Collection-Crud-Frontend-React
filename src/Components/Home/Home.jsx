335df6a6076bf4f355310e45b4e6de8b9c9334e3
mdselimme <mdselimdev@gmail.com>
2024-12-09 20:38:13 +0600

    'blood-donar-information-form

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,53 @@
 const Home = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-center py-14">Enter your data Here</h1>
-      <div className="text-center">
+      <h1 className="text-center py-8 text-3xl font-bold text-[#1B2247]">
+        Enter your Information Here
+      </h1>
+      <div className="text-center w-3/4 mx-auto">
         <form>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
+          <div className="grid grid-cols-2 gap-10 mb-8">
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter your full name"
+              className="input input-bordered w-full"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-10 mb-10">
+            <input
+              type="number"
+              name="phonenumber"
+              placeholder="Enter your phone number"
+              className="input input-bordered w-full"
+            />
+            <input
+              type="number"
+              name="weight"
+              placeholder="Enter your Weight"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-10 mb-10">
+            <input
+              type="number"
+              name="height"
+              placeholder="Enter your Height"
+              className="input input-bordered w-full"
+            />
+            <input
+              type="number"
+              name="age"
+              placeholder="Enter your age 18 - 50"
+              className="input input-bordered w-full"
+            />
+          </div>
         </form>
       </div>
     </div>
